ff774b3388c9febfb9e1d44e7ce152dced589836
Partillay <thegrandmowdowner@gmail.com>
2025-01-23 17:01:05 +0800

    Deleted/Rename original file and Added a greeting

---
+++ src/main/java/Partillay.java
@@ -0,0 +1,17 @@
+public class Partillay {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello! I'm Partillay, your greatest bestie.");
+        System.out.println("What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+
+        System.out.println("Bye. See you later!");
+        System.out.println(HORIZONTAL_LINE);
+
+    }
+
+}
+
