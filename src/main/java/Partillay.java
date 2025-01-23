1b7398da6fa353ed58717dfcd8ea402b82a7bfd1
Partillay <thegrandmowdowner@gmail.com>
2025-01-23 18:24:10 +0800

    Added echoes

--- src/main/java/Partillay.java
+++ src/main/java/Partillay.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
+
 public class Partillay {
     private static final String HORIZONTAL_LINE = "____________________________________________________________";
 
     public static void main(String[] args) {
 
         System.out.println(HORIZONTAL_LINE);
-        System.out.println("Hello! I'm Partillay, your greatest bestie.");
-        System.out.println("What can I do for you?");
+        System.out.println("Hello! I'm Partillay, your ABSOLUTELY greatest bestie.");
+        System.out.println("Anything I CAN HELP?");
         System.out.println(HORIZONTAL_LINE);
 
-        System.out.println("Bye. See you later!");
-        System.out.println(HORIZONTAL_LINE);
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String userInput = sc.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("Bye. See you later! Slay!");
+                System.out.println(HORIZONTAL_LINE);
+                break;
+            }
+
+            System.out.println(HORIZONTAL_LINE);
+            System.out.println("I got it! You said " + userInput);
+            System.out.println(HORIZONTAL_LINE);
+
+        }
+
+        sc.close();
 
     }
 
