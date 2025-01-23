204f0479aec2d7f0f2d9cd430a45c1b1cc627ea6
Partillay <thegrandmowdowner@gmail.com>
2025-01-23 23:12:43 +0800

    Revert "added support for add,list"
    This reverts commit 8619905df43a2d9dd5678d5c72e6c44e0c7135d2.

--- src/main/java/Partillay.java
+++ src/main/java/Partillay.java
@@ -5,9 +5,6 @@ public class Partillay {
 
     public static void main(String[] args) {
 
-        String[] tasks = new String[100];
-        int taskIndex = 0;
-
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm Partillay, your ABSOLUTELY greatest bestie.");
         System.out.println("Anything I CAN HELP?");
@@ -18,26 +15,15 @@ public class Partillay {
         while (true) {
             String userInput = sc.nextLine();
 
-            System.out.println(HORIZONTAL_LINE);
-
             if (userInput.equals("bye")) {
+                System.out.println(HORIZONTAL_LINE);
                 System.out.println("Bye. See you later! Slay!");
                 System.out.println(HORIZONTAL_LINE);
                 break;
             }
 
-            if (userInput.equals("list")) {
-                for (int i = 0; i < taskIndex; i++) {
-                    System.out.println(i + 1 + ". " + tasks[i]);
-                }
-                System.out.println(HORIZONTAL_LINE);
-                continue;
-
-            }
-
-            System.out.println("added: " + userInput);
-            tasks[taskIndex] = userInput;
-            taskIndex++;
+            System.out.println(HORIZONTAL_LINE);
+            System.out.println("I got it! You said " + userInput);
             System.out.println(HORIZONTAL_LINE);
 
         }
